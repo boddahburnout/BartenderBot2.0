3a2c3b39d5b614e3768a63e04243e23b0aebca8a
Dakota Vega <dvega9876@gmail.com>
2021-05-01 21:41:41 -0400

    Updated Mention handler to reflect the new static prefix

--- src/main/java/darth/bartenderbot/handler/MentionedHandler.java
+++ src/main/java/darth/bartenderbot/handler/MentionedHandler.java
@@ -13,17 +13,19 @@ public class MentionedHandler {
         TextChannel channel = message.getTextChannel();
         Member member = message.getMember();
         JDA jda = channel.getJDA();
-        try {
-            YamlFile botConfig = new ConfigManager().accessConfig();
+//        try {
+//            YamlFile botConfig = new ConfigManager().accessConfig();
             if (message.getContentRaw().contains("prefix")) {
-                if (message.getContentRaw().split(" ")[1].equalsIgnoreCase("prefix")) {
-                    channel.sendMessage("My prefix is set to " + botConfig.get(guild.getId() + ".Prefix")).queue();
-                    return;
-                }
+//                if (message.getContentRaw().split(" ")[1].equalsIgnoreCase("prefix")) {
+//                    channel.sendMessage("My prefix is set to " + botConfig.get(guild.getId() + ".Prefix")).queue();
+//                    return;
+//                }
+//            }
+//        } catch (InvalidConfigurationException | IOException e) {
+//            e.printStackTrace();
+//        }
+                channel.sendMessage("My prefix is ~").queue();
             }
-        } catch (InvalidConfigurationException | IOException e) {
-            e.printStackTrace();
-        }
         try {
             new CleverbotHandler().sendRequest(guild, "697355371056201858", channel, message, member, jda.getRoles());
         } catch (InvalidConfigurationException e) {
